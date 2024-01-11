b87ac8ec6a514b532199c0178d5dcd56b782121b
joeokivie <josephokivie@hotmail.com>
2024-01-11 04:05:25 -0500

    push new code

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
+// Home route
+router.get('/', (req, res) => {
+  return res.send('Welcome to the home route!');
+});
+
+// API routes
 router.use('/api', apiRoutes);
 
-router.use((req, res) => res.send('Wrong route!'));
+// Default route for any other path
+router.use((req, res) => {
+  return res.send('Wrong route!');
+});
 
 module.exports = router;
